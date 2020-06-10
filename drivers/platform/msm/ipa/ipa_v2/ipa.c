c3ce56e2fd049460901ab01f73ca2efafffb0e43
itsvixano <vixanonew@gmail.com>
2020-06-10 13:47:26 +0000

    msm: ipa: Do not acquire wakelocks
    IPA's wakelocks are the cause for some minor idle drain on Android
    devices.
    
    kang from https://github.com/SuperiorOS-Devices/kernel_xiaomi_lavender/commit/2be60b9881e97322c86e5f2d4b172654a6faa43b
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: itsvixano <vixanonew@gmail.com>
    Signed-off-by: starlight5234 <ifist2834@gmail.com>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>
    Signed-off-by: Hadad <repo-sync@outlook.com>
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- drivers/platform/msm/ipa/ipa_v2/ipa.c
+++ drivers/platform/msm/ipa/ipa_v2/ipa.c
@@ -3499,10 +3499,6 @@ void ipa_inc_acquire_wakelock(enum ipa_wakelock_ref_client ref_client)
 		IPAERR("client enum %d mask already set. ref cnt = %d\n",
 		ref_client, ipa_ctx->wakelock_ref_cnt.cnt);
 	ipa_ctx->wakelock_ref_cnt.cnt |= (1 << ref_client);
-	if (ipa_ctx->wakelock_ref_cnt.cnt &&
-		!ipa_ctx->wakelock_ref_cnt.wakelock_acquired) {
-		ipa_ctx->wakelock_ref_cnt.wakelock_acquired = true;
-	}
 	IPADBG_LOW("active wakelock ref cnt = %d client enum %d\n",
 		ipa_ctx->wakelock_ref_cnt.cnt, ref_client);
 	spin_unlock_irqrestore(&ipa_ctx->wakelock_ref_cnt.spinlock, flags);
@@ -3526,10 +3522,6 @@ void ipa_dec_release_wakelock(enum ipa_wakelock_ref_client ref_client)
 	ipa_ctx->wakelock_ref_cnt.cnt &= ~(1 << ref_client);
 	IPADBG_LOW("active wakelock ref cnt = %d client enum %d\n",
 		ipa_ctx->wakelock_ref_cnt.cnt, ref_client);
-	if (ipa_ctx->wakelock_ref_cnt.cnt == 0 &&
-		ipa_ctx->wakelock_ref_cnt.wakelock_acquired) {
-		ipa_ctx->wakelock_ref_cnt.wakelock_acquired = false;
-	}
 	spin_unlock_irqrestore(&ipa_ctx->wakelock_ref_cnt.spinlock, flags);
 }
 
